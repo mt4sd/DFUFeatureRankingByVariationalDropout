e3cbf62ea8b126dc66c26409e1f00b488ccf73cd
Abián Hernández <abianhernandez@outlook.es>
2022-09-21 15:09:09 +0100

    Features completed

--- config.py
+++ config.py
@@ -1,1 +1,13 @@
 INAOE_DATASET_DIR = '/home/abian/Data/Dataset/GTMA/DiabeticFootDataset/INAOE/Data/'
+
+import numpy as np
+def sliding_window(a, window=2):
+    shape = a.shape[:-1] + (a.shape[-1] - window + 1, window)
+    strides = a.strides + (a.strides[-1],)
+    return np.lib.stride_tricks.as_strided(a, shape=shape, strides=strides)
+
+# initial_temperature = 15
+# EXPECTED_VALUE_INTERVAL = np.cumsum([0, 3.25, 3.3, 3.3, 3.3, 3.3, 3.3, 3.25]) + initial_temperature
+initial_temperature = 18
+EXPECTED_VALUE_INTERVAL = np.cumsum([0, 4, 4, 1, 1, 1, 1, 1, 1, 1, 4]) + initial_temperature
+EXPECTED_VALUE_CLUSTER = sliding_window(EXPECTED_VALUE_INTERVAL, 2).mean(axis=1)
---
+++ notes.md
@@ -0,0 +1,14 @@
+# Control Group
+min: 15.98
+max: 33.55
+
+# DM Group
+min: 18.05
+max: 37.28
+
+## Intervals
+bins_width: [3.25, 3.3, 3.3, 3.3, 3.3, 3.3, 3.25]
+bins_range: [15, 18.25, 21.55, 24.85, 28.15, 31.45, 34.75, 38.]
+
+# Thermal Change Index
+Hernandez-Contreras, D., Peregrina-Barreto, H., Rangel-Magdaleno, J., Gonzalez-Bernal, J. A., & Altamirano-Robles, L. (2017). A quantitative index for classification of plantar thermal changes in the diabetic foot. Infrared Physics & Technology, 81, 242-249.
