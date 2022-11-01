0fa290b167f4db31318dfe4c79058c3dcea50cfc
Abián Hernández <abianhernandez@outlook.es>
2022-11-01 16:25:21 +0900

    Feature selection Experiments

--- dataset/FeaturesDataset.py
+++ dataset/FeaturesDataset.py
@@ -4,7 +4,7 @@ import os
 import numpy as np
 
 class FeaturesDataset(Dataset):
-    def __init__(self, dataset_dir: str):
+    def __init__(self, dataset_dir: str, normalize: bool = True):
         super(FeaturesDataset, self).__init__()
         X_df = pd.read_csv(os.path.join(dataset_dir, 'dfu_features_dataset_selected.csv'), index_col=0)
         y_df = pd.read_csv(os.path.join(dataset_dir, 'dfu_labels_dataset.csv'), index_col=0)
@@ -13,8 +13,12 @@ class FeaturesDataset(Dataset):
         self.X = X_df.to_numpy().astype(np.float32)
         self.y = y_df.to_numpy().ravel()
 
+        if normalize:
+            self.X = (self.X - self.X.mean(axis=0)) / self.X.std(axis=0)
+
     def __len__(self):
         return len(self.X)
 
     def __getitem__(self, idx):
         return self.X[idx], self.y[idx]
+
